ec82488c44596203c2e6222cce6b201aa07a9384
Mickael Guevel <127212295+Ultrax92@users.noreply.github.com>
2025-12-16 16:49:16 +0100

    Activité 13

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,19 +3,23 @@
 @section('title', 'Accueil')
 
 @section('content')
-    <h2>Bienvenue sur le site de {{ $name }}</h2>
-    
-    <p>Voici nos derniers articles :</p>
-    <br>
+<h2>Bienvenue sur le site de {{ $name }}</h2>
 
-    <div class="articles-list">
-        
-        @foreach ($articles as $article)
-            <x-article 
-                :title="$article['title']" 
-                :description="$article['description']" 
-            />
-        @endforeach
+<p>Voici nos derniers articles :</p>
+<br>
 
-    </div>
+<div class="articles-list">
+
+    @foreach ($articles as $article)
+
+    @if ($loop->last)
+    @break
+    @endif
+
+    <x-article
+        :title="$article['title']"
+        :description="$article['description']" />
+    @endforeach
+
+</div>
 @endsection

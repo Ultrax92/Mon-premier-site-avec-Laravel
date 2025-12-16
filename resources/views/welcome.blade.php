2bb6b84b0014a99eb2588c2cea37b921eab85da7
Mickael Guevel <127212295+Ultrax92@users.noreply.github.com>
2025-12-16 16:41:04 +0100

    Activité 12

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,24 +3,19 @@
 @section('title', 'Accueil')
 
 @section('content')
-<h2>Bienvenue sur le site de {{ $name }}</h2>
+    <h2>Bienvenue sur le site de {{ $name }}</h2>
+    
+    <p>Voici nos derniers articles :</p>
+    <br>
 
-<p>Voici nos derniers articles :</p>
-<br>
+    <div class="articles-list">
+        
+        @foreach ($articles as $article)
+            <x-article 
+                :title="$article['title']" 
+                :description="$article['description']" 
+            />
+        @endforeach
 
-<div class="articles-list">
-
-    <x-article
-        title="L’IA soigne mieux"
-        description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite." />
-
-    <x-article
-        title="Villes vertes"
-        description="Les métropoles deviennent plus écologiques et durables." />
-
-    <x-article
-        title="Télétravail"
-        description="Plus de liberté, mais aussi plus de solitude." />
-
-</div>
+    </div>
 @endsection

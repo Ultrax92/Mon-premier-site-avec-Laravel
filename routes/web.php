87d573e16abeed4ee965efd000e3066ed57fcdc2
Mickael Guevel <127212295+Ultrax92@users.noreply.github.com>
2025-12-16 11:29:02 +0100

    Activité 7

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/', [IndexController::class, 'index']);
+Route::get('/', [IndexController::class, 'index'])->name('home');
 
-Route::get('/articles/{id}', [ArticleController::class, 'show']);
+Route::get('/articles/{id}', [ArticleController::class, 'show'])->name('article.details');

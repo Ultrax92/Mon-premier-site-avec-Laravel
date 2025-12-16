b37ca1497da4b24c5073b1b14d069438c05e83b8
Mickael Guevel <127212295+Ultrax92@users.noreply.github.com>
2025-12-16 14:08:45 +0100

    Fix welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,24 +3,24 @@
 @section('title', 'Accueil')
 
 @section('content')
-    <h2>Bienvenue sur la page d’accueil</h2>
-    
-    <div class="articles-list">
-        
-        <x-article 
-            title="L’IA soigne mieux" 
-            description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite." 
-        />
+<h2>Bienvenue sur le site de {{ $name }}</h2>
 
-        <x-article 
-            title="Villes vertes" 
-            description="Les métropoles deviennent plus écologiques et durables." 
-        />
+<p>Voici nos derniers articles :</p>
+<br>
 
-        <x-article 
-            title="Télétravail" 
-            description="Plus de liberté, mais aussi plus de solitude." 
-        />
+<div class="articles-list">
 
-    </div>
+    <x-article
+        title="L’IA soigne mieux"
+        description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite." />
+
+    <x-article
+        title="Villes vertes"
+        description="Les métropoles deviennent plus écologiques et durables." />
+
+    <x-article
+        title="Télétravail"
+        description="Plus de liberté, mais aussi plus de solitude." />
+
+</div>
 @endsection

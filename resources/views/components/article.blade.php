13d853a5e6d1f61022fc88154066d4cb0883a67b
Mickael Guevel <127212295+Ultrax92@users.noreply.github.com>
2025-12-16 13:45:26 +0100

    Activité 10 & 11

---
+++ resources/views/components/article.blade.php
@@ -0,0 +1,4 @@
+<div class="article-card" style="border: 1px solid #ccc; padding: 15px; margin-bottom: 10px; border-radius: 5px;">
+    <h3>{{ $title }}</h3>
+    <p>{{ $description }}</p>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,5 +3,24 @@
 @section('title', 'Accueil')
 
 @section('content')
-    <h1>Bienvenue sur le site de {{ $name }}</h1>
+    <h2>Bienvenue sur la page d’accueil</h2>
+    
+    <div class="articles-list">
+        
+        <x-article 
+            title="L’IA soigne mieux" 
+            description="L’intelligence artificielle aide les médecins à diagnostiquer plus vite." 
+        />
+
+        <x-article 
+            title="Villes vertes" 
+            description="Les métropoles deviennent plus écologiques et durables." 
+        />
+
+        <x-article 
+            title="Télétravail" 
+            description="Plus de liberté, mais aussi plus de solitude." 
+        />
+
+    </div>
 @endsection
